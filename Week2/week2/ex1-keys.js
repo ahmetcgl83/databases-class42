c579ea01d2567f4f4b4c3f5385414b08ea712b9c
Ahmet Cigil <ahmetcgln@gmail.com>
2023-03-29 16:50:00 +0200

    update ex1

--- Week2/week2/ex1-keys.js
+++ Week2/week2/ex1-keys.js
@@ -5,7 +5,6 @@ const db = mysql.createConnection({
 	host: "localhost",
 	user: "hyfuser",
 	password: "hyfpassword",
-	multipleStatements: true,
 });
 
 db.connect((err) => {
@@ -15,22 +14,21 @@ db.connect((err) => {
 	console.log("MySql connected...");
 });
 
-db.query("CREATE DATABASE IF NOT EXISTS db", (err, result) => {
-	if (err) throw err;
-	console.log(result);
-});
+db.query("CREATE DATABASE IF NOT EXISTS db_week2");
 
-db.query("USE db", (err, result) => {
+db.query("USE db_week2", (err, result) => {
 	if (err) throw err;
 	console.log(result);
 	console.log("Connected to db...");
 });
 
+db.query("DROP TABLE IF EXISTS authors");
+
 db.query(
 	`CREATE TABLE authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(25),
-    university VARCHAR(50),
+    author_name VARCHAR(255),
+    university VARCHAR(255),
     date_of_birth DATE,
     h_index INT,
     gender VARCHAR(10))
